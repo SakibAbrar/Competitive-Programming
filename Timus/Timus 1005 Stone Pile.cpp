d59bce0b4cc0dcf55124f65690f19f708ec8165f
Sakib Abrar <sakib.cghs@gmail.com>
2019-04-23 23:18:15 +0600

    Create Timus 1005 Stone Pile.cpp

---
+++ Timus/Timus 1005 Stone Pile.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int weight[25];
+bool possible[2000005];
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    possible[0] = true;
+
+    for(int idx = 0; idx<n; idx++){
+        cin >> weight[idx];
+        sum += weight[idx];
+    }
+
+    int ans = sum;
+
+    possible[0] = true;
+
+    for(int idx = 0; idx<n; idx++){
+        for(int wt = sum;  wt>=1; wt--){
+
+            if(possible[wt-weight[idx]] && wt>=weight[idx]){
+                possible[wt] = true;
+                ans = min(ans, abs(sum-2*wt));
+
+            }
+        }
+
+    }
+
+    cout << ans << endl;
+
+}

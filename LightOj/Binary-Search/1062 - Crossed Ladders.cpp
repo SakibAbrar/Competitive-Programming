56dec275635da45b87ac9e6ef77fb5ca43b42be2
Sakib Abrar <sakib.cghs@gmail.com>
2019-10-28 20:54:53 +0600

    Create 1062 - Crossed Ladders.cpp

---
+++ LightOj/Binary-Search/1062 - Crossed Ladders.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double x, y, c, b, ansH, low, hi, mid;
+
+
+double binary_search(){
+    low = 0.0;
+    hi = min(x, y);
+
+    for(int i = 0; i<200; i++){
+
+        mid = (low + hi)/2.0;
+        b = mid; 
+        
+        ansH = ( sqrt(x*x-b*b) * sqrt(y*y-b*b) )/( sqrt(x*x-b*b) + sqrt(y*y-b*b) );
+        // xy/(x+y)
+
+        if( ansH < c)
+            hi = mid;
+        else 
+            low = mid;
+    }
+
+    return mid;
+
+}
+
+int main()
+{   
+    //freopen( "output.txt", "w", stdout);
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
+    int n, caseno = 0, cases;
+    scanf("%d", &cases);
+    while(cases-- ) {
+        scanf("%lf %lf %lf", &x, &y, &c);
+        printf("Case %d: %0.10lf\n", ++caseno, binary_search());
+    }
+
+    return 0;
+}

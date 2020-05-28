0f0604735a27c88375291bf40a79e68629863848
Sakib Abrar <sakib.cghs@gmail.com>
2020-05-28 10:48:02 +0600

    Create 12034 - Race.cpp

---
+++ Uva Oj/DP/12034 - Race.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int T, n, cases;
+int mod = 10056;
+int dp[1005];
+int binom[1005][1005];
+
+void binomial() {
+
+    for (int n = 1; n <= 1002; n ++) {
+        for (int k = 0; k <= 1002; k ++) {
+            if (n < k) 
+                binom[n][k] = 0;
+            else if (n == k || k == 0)
+                binom[n][k] = 1;
+            else 
+                binom[n][k] = (binom[n - 1][k - 1] + binom[n -1][k]) % mod;
+        }
+    }
+
+}
+
+int solve(int horses) {
+    // base
+    if (horses == 0)
+        return dp[horses] = 1;
+
+    // memo
+    if (dp[horses]) 
+        return dp[horses];
+
+    // reccurence
+    int ret = 0;
+    for (int first = 1; first <= horses; first ++) {
+        ret += ( binom[horses][first] * solve(horses - first) ) % mod;
+        ret %= mod;
+    }
+    
+    return dp[horses] = ret;
+}
+
+
+int main() {
+    freopen("output.txt", "w", stdout);
+    scanf("%d", &T);
+
+    binomial();
+    solve(1002);
+    
+
+    while(cases < T) {
+        scanf("%d", &n);
+        printf("Case %d: %d\n", ++cases, dp[n]);
+    }
+    
+
+    return 0;
+}
